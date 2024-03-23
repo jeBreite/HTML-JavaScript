980bbe8a5d8e04158148f4380a4aad93fb7634ad
Jenni Breite <99127259+jeBreite@users.noreply.github.com>
2024-03-23 10:18:13 +0200

    add AnkkalinnanVaalit folder

---
+++ AnkkalinnanVaalit/ankkalinnaVaalit.mjs
@@ -0,0 +1,27 @@
+//alustetaan muuttujat
+let luku2 = 0;
+let luku3 = 0;
+let luku4 = 0;
+let luku5 = 0;
+let luku6 = 0; 
+	function kakkonen(){
+		luku2++;
+		document.getElementById("tulostus2").innerHTML = "2. Aku Ankka " + luku2;
+	}
+	function kolmonen(){
+		luku3++; 
+		document.getElementById("tulostus3").innerHTML = "3. Roope Setä " + luku3;
+    }
+    function nelonen(){
+		luku4++; 
+		document.getElementById("tulostus4").innerHTML = "4. Minni Hiiri " + luku4;
+    }
+    function vitonen(){
+		luku5++; 
+		luku5++;
+		document.getElementById("tulostus5").innerHTML = "5. Hannu Hanhi " + luku5;
+    }
+    function kutonen(){
+		luku6++; 
+		document.getElementById("tulostus6").innerHTML = "6. Pelle Peloton " + luku6;
+	}
